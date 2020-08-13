eb0a2a027e3c95bf82341102d6fcb01aee8eb301
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-08-13 11:05:43 +0900

    Update One_Hot_Encoding_02.py

--- AI/One_Hot_Encoding_02.py
+++ AI/One_Hot_Encoding_02.py
@@ -12,7 +12,7 @@ t.fit_on_texts([text])
 print(t.word_index)
 
 sub_text = "점심 먹으러 갈래 메뉴는 햄버거 최고야"
-encoded = t.texts_to_squences([sub_text])[0]
+encoded = t.texts_to_sequences([sub_text])[0]
 print(encoded)
 
 # 원-핫 인코딩 수행
