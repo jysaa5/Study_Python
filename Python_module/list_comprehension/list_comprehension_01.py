d09a35ed7045dc622c6d95e9aaad75283f82cf6a
jysaa5 <jysaa5@naver.com>
2021-03-04 03:20:30 +0900

    Update list_comprehension_01.py

---
+++ Python_module/list_comprehension/list_comprehension_01.py
@@ -0,0 +1,22 @@
+# list comprehension
+# 리스트
+print([n*2 for n in range(1, 10+1) if n%2 == 1])
+
+a = [n*2 for n in range(1, 10+1) if n%2 == 1]
+print(a)
+
+a = []
+for n in range(1, 10+1):
+    if n%2 == 1:
+        a.append(n*2)
+print(a)
+
+# 딕셔너리
+b = {"a": 1, "b": 2}
+print({value:key for key,value in b.items()})
+
+a = {}
+for key, value in b.items():
+    a[value] = key
+print(a)
+
