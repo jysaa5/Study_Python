729539de3410a306886054307083044f88e87757
jysaa5 <jysaa5@naver.com>
2021-03-28 20:52:31 +0900

    Solve Problem

--- BaekJoo_Ex/Basic_Math_02/Taxicab_Geometry_01.py
+++ BaekJoo_Ex/Basic_Math_02/Taxicab_Geometry_01.py
@@ -1,4 +1,5 @@
 # 3053번: 택시 기하학
+# solution 1
 import math
 r = int(input())
 print(r*r*math.pi)
---
+++ BaekJoo_Ex/Basic_Math_02/Turret_01.py
@@ -0,0 +1,11 @@
+# 1002번: 터렛
+# solution 1
+t = int(input())
+
+for i in range(t):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    r = ((x1 - x2)**2 + (y1 - y2)**2) ** (1/2)
+    R = [r1, r2, r]
+    m = max(R)
+    R.remove(m)
+    print(-1 if (r == 0 and r1 == r2) else 1 if (r == r1+r2 or m == sum(R)) else 0 if(m > sum(R)) else 2)
