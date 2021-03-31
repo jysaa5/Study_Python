fa3d370e3ab9ad740e484f4c8082cefd688b3157
jysaa5 <jysaa5@naver.com>
2021-04-01 00:01:35 +0900

    Update start_picking_01.py

--- BaekJoo_Ex/Recursion/start_picking_01.py
+++ BaekJoo_Ex/Recursion/start_picking_01.py
@@ -1,8 +1,23 @@
 # 2447번: 별 찍기 - 10
 
-n = int(input())
+def get_stars(n):
+    matrix = []
+    for i in range(3 * len(n)):
+        if i // len(n) == 1:
+            matrix.append(n[i % len(n)] + " " * len(n) + n[i % len(n)])
+        else:
+            matrix.append(n[i % len(n)] * 3)
+    return matrix
 
-if n == 3:
-    print('***')
-    print('* *')
-    print('***')
+
+star = ["***", "* *", "***"]
+n = int(input())
+e = 0
+while n != 3:
+    n = int(n / 3)
+e += 1
+
+for i in range(e):
+    star = get_stars(star)
+for i in star:
+    print(i)
