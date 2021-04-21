a8559a98eb7ebe728413230396ed3aacda403e4a
jysaa5 <jysaa5@naver.com>
2021-04-22 01:56:57 +0900

    Study Python

--- Coding_Test/Shadow_connection.py
+++ Coding_Test/Shadow_connection.py
@@ -17,6 +17,7 @@ graph = {100: set([67, 66]),
          31: set([87]),
          78: set([87])}
 
+
 def 깊이우선탐색(graph, start):
     방문 = []
     stack = [start]
@@ -25,9 +26,25 @@ def 깊이우선탐색(graph, start):
         if n not in 방문:
             방문.append(n)
             차집합 = graph[n]-set(방문)
-            stack += 차집합
+            # 마지막 노드 끝까지 갔을 때
+            if len(차집합) == 0:
+                # 마지막 원소 추가
+                방문 += stack
+                break
+            #stack.append(min(차집합))
+            stack.append(max(차집합))
             print(f'visited: {방문}')
             print(f'stack:{stack}')
     return 방문
 
+
 깊이우선탐색(graph, 100)
+
+# 아스키코드로 변환
+# min: 100, 66, 69, 65, 84
+# max: 100, 67, 82, 79, 87, 78
+
+print('----------------------------------------------')
+print(''.join([chr(i) for i in [66, 69, 65, 84]]))
+print(''.join([chr(i) for i in [67, 82, 79, 87, 78]]))
+
